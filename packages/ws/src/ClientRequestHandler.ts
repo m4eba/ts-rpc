380b23391010afbc05510ac991f815fee00848ce
m4eba <m4eba.0@gmail.com>
2020-01-24 23:06:02 +0100

    add ClientRequestHandler

---
+++ packages/ws/src/ClientRequestHandler.ts
@@ -0,0 +1,65 @@
+import { PacketHandler, RequestPacket, ResponsePacket, PacketSender, ErrorPacket } from "./Packet";
+
+interface Request {
+  id: number;
+  resolve: (response: ResponsePacket) => void;
+  reject: (error: any) => void;
+  timeout: NodeJS.Timeout;
+}
+class ClientRequestHandler implements PacketHandler {
+  private sender: PacketSender;
+  private idCount: number = 1;
+  private timeout = 3 * 60 * 1000;
+  private idMap: Map<number, Request>;
+
+  constructor(sender: PacketSender) {
+    this.sender = sender;
+    this.idMap = new Map();
+  }
+
+  public request(packet: RequestPacket): Promise<ResponsePacket> {
+    packet.id = this.idCount++;
+    this.sender.send(packet);
+    const result = new Promise<ResponsePacket>((resolve, reject) => {
+      this.idMap.set(packet.id, {
+        id: packet.id,
+        resolve,
+        reject,
+        timeout: setTimeout(() => {
+          this.requestTimeout(packet.id);
+        }, this.timeout)
+      });
+    });
+    return result;
+  }
+
+  public onError(packet: ErrorPacket): void {
+    const req = this.idMap.get(packet.id);
+    if (req === undefined) {
+      return;
+    }
+    req.reject(packet.error);
+    this.idMap.delete(packet.id);
+  }
+  public onEvent(): void { }
+  public onRequest(): void { }
+  public onResponse(packet: ResponsePacket): void {
+    const req = this.idMap.get(packet.id);
+    if (req === undefined) {
+      return;
+    }
+    req.resolve(packet);
+    this.idMap.delete(packet.id);
+  }
+
+  public requestTimeout(id: number): void {
+    const req = this.idMap.get(id);
+    if (req === undefined) {
+      return;
+    }
+    req.reject('timeout');
+    this.idMap.delete(id);
+  }
+}
+
+export default ClientRequestHandler;
--- packages/ws/src/ws.ts
+++ packages/ws/src/ws.ts
@@ -1,4 +1,5 @@
 export * from './Client';
+export * from './ClientRequestHandler';
 export * from './Server';
 export * from './ServerHandler';
 export * from './Packet';
